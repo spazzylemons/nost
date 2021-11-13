1a2355bec58fe34dbe28dbac141e5b1e57407cfa
danielkline10 <danielkline@Daniels-MacBook-Pro-2.local>
2021-11-13 15:33:25 -0500

    added login functionality

--- backend_django/nostAPIs/views.py
+++ backend_django/nostAPIs/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import (
+    MyTokenObtainPairSerializer,
+    CustomUserSerializer,
+)
+from rest_framework import status, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 # Create your views here.
+
+
+class ObtainTokenPairView(TokenObtainPairView):
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = MyTokenObtainPairSerializer
+
+
+class CustomUserCreate(APIView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format='json'):
+        serializer = CustomUserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                json = serializer.data
+                return Response(json, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
